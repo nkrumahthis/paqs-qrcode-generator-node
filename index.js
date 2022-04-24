a3ff48da7aa26adde5c31bceae43dd48ce3e2687
Nkrumah <nkrumahthis@gmail.com>
2022-04-24 10:51:47 +0000

    connect to database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const checkerController = require('./controllers/checker.controller');
 const generatorController = require('./controllers/generator.controller');
 const homeController = require('./controllers/home.controller');
+const { connectToDB } = require('./lib/database.lib');
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,14 @@ app.get("/", homeController.get);
 app.get("/checker", checkerController.get);
 app.post("/generator", generatorController.post)
 
-app.listen(PORT, () => {
-    console.log("PAQS-QRCode-Generator is listening on port " + PORT)
-});
+async function main() {
+    await connectToDB().catch(error => {
+        console.error('error connecting to db', error.message)
+    })
+
+    app.listen(PORT, () => {
+        console.log("PAQS-QRCode-Generator is listening on port " + PORT)
+    });
+}
+
+main();
