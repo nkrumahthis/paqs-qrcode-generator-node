385b9a1d13d14dbb563b665d6a313070af7c9000
Nkrumah <nkrumahthis@gmail.com>
2022-04-24 11:38:44 +0000

    generate qrs

--- controllers/generator.controller.js
+++ controllers/generator.controller.js
@@ -1,4 +1,5 @@
 const { generateItems } = require("../lib/item.lib");
+const { generateQRs } = require("../lib/qr.lib")
 
 async function post(req, res) {
     const data = req.body;
@@ -9,16 +10,30 @@ async function post(req, res) {
     const mfgDate = data.mfgDate;
     const expDate = data.expDate;
     const quantity = data.quantity;
+    const format = data.format;
+
+    if (!validate(data)) {
+        res.status(500).send({ "message": "invalid data entered. please check your post body" })
+        return;
+    }
 
     const itemIds = await generateItems(productName, companyName, logo, mfgDate, expDate, quantity);
-    // const qrImages = generateQRs(itemIds);
+    const qrImages = await generateQRs(itemIds, format);
     // const zipFile = zipUp(qrImages);
 
     // res.sendFile(zipFile, err => {
     //     console.error(err)
     // })
 
-    res.send(itemIds)
+    res.send(qrImages)
+}
+
+function validate(data) {
+    if (data.format == "" || data.format == undefined) {
+        console.error("data format absent")
+        return false
+    }
+    return true;
 }
 
 module.exports.post = post;
